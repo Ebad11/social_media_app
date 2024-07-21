aa561a0fe20dd02ec5f8b3e6a8c9507c17a978c0
Ebad11 <ebadshk48@gmail.com>
2024-07-21 11:07:16 +0530

    first commit

---
+++ components/layout/BottomBar.jsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { sidebarLinks } from '../../constants/index'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link';
+
+const BottomBar = () => {
+  const pathName = usePathname()
+  return (
+    <div className="flex bottom-0 z-20 w-full bg-dark-1 px-6 py-3 items-center justify-between md:hidden">
+      {sidebarLinks.map((link) => {
+        const isActive = pathName === link.route;
+
+        return (
+          <Link key={link.label} href={link.route} className={`flex gap-2 items-center  rounded-lg py-2 px-4 ${isActive && 'bg-purple-1'}`} >
+            {link.icon} <p className=' text-subtle-medium text-light-1  max-sm:hidden'>{link.label}</p>
+          </Link>
+        )
+      })}
+    </div>
+  )
+}
+
+export default BottomBar;
---
+++ components/layout/TopBar.jsx
@@ -0,0 +1,61 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import { Add, Logout, Search } from '@mui/icons-material'
+import { useRouter } from 'next/navigation'
+import { SignedIn, SignOutButton } from '@clerk/nextjs'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const TopBar = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState('')
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768);
+    };
+
+    handleResize(); // Check screen size on initial load
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <div className='flex gap-[2%] items-center mt-6'>
+
+      <div className='relative w-[74%] max-md:w-[80%]'>
+        <input type="text" className='search-bar' placeholder='Search Posts, People, ...' value={search} onChange={(e) => setSearch(e.target.value)} />
+        <Search className='search-icon' onClick={() => { }} />
+      </div>
+
+      <button className='create-post-btn whitespace-nowrap w-[24%]' onClick={() => { router.push("/create-post") }}>
+        <Add /><span>Create</span>
+      </button>
+      {
+        isSmallScreen &&
+        <div className="flex gap-3">
+        <SignedIn>
+          <SignOutButton>
+            <div className='cursor-pointer flex md:hidden items-center'>
+              <Logout sx={{ color: "white", fontSize: '32px' }} />
+            </div>
+          </SignOutButton>
+        </SignedIn>
+
+
+        <Link href="/">
+          <Image src='/assets/phucmai.png' alt='profile-pic' width={50} height={50} className='rounded-full md:hidden'></Image>
+        </Link>
+      </div>
+      }
+
+    </div>
+  )
+}
+
+export default TopBar
