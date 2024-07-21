55681f619881b5c4585ba508ea60480e49d4aa36
Ebad11 <ebadshk48@gmail.com>
2024-07-21 14:20:41 +0530

    debugger removed

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -7,7 +7,6 @@ export async function POST(req) {
 
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
-   debugger;
   if (!WEBHOOK_SECRET) {
     throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
