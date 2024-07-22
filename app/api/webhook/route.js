d976aa76c9a753146c2ec361a142612e52126507
Ebad11 <ebadshk48@gmail.com>
2024-07-22 09:32:39 +0530

    responses updated

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -1,16 +1,16 @@
-// app/api/webhook/route.js
-import { Webhook } from "svix";
-import { headers } from "next/headers";
-import { createOrUpdateUser, deleteUser } from "@lib/actions/user";
+import { Webhook } from 'svix'
+import { headers } from 'next/headers'
+import { WebhookEvent } from '@clerk/nextjs/server'
+import { createOrUpdateUser } from '../../../lib/actions/user'
 
 export async function POST(req) {
-  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+    console.log("Alert received")
+
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
   if (!WEBHOOK_SECRET) {
-    throw new Error(
-      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
-    );
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
   }
 
   // Get the headers
@@ -21,13 +21,13 @@ export async function POST(req) {
 
   // If there are no headers, error out
   if (!svix_id || !svix_timestamp || !svix_signature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
+    return new Response('Error occured -- no svix headers', {
+      status: 400
+    })
   }
 
   // Get the body
-  const payload = await req.json();
+  const payload = await req.json()
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
@@ -41,54 +41,49 @@ export async function POST(req) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    });
+    })
   } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured in verifying webhook", {
-      status: 400,
-    });
+    console.error('Error verifying webhook:', err);
+    return new Response('Error occured in verifying webhooks', {
+      status: 400
+    })
   }
 
-  // Handle the event
-  const eventType = evt?.type;
+ const eventType = evt?.type;
 
-  if (eventType === "user.created" || eventType === "user.updated") {
-    const { id, first_name, last_name, image_url, email_addresses, username } = evt?.data;
+ if(eventType === 'user.created' || eventType === 'user.updated')
+ {
+    const{id, first_name, last_name, image_url, email_addresses, username} = evt?.data;
 
-    try {
-      await createOrUpdateUser(
-        id,
-        first_name,
-        last_name,
-        image_url,
-        email_addresses,
-        username
-      );
+    try
+    {
+        await createOrUpdateUser(id, first_name, last_name, image_url, email_addresses, username)
 
-      return new Response("User is created or updated", {
-        status: 200,
-      });
-    } catch (err) {
-      console.error("Error creating or updating user:", err);
-      return new Response("Error occured in creating user", {
-        status: 500, 
-      });
+        return new Response('User created or updated', {status: 200});
     }
-  }
 
-  if (eventType === "user.deleted") {
-    try {
-      const { id } = evt?.data;
-      await deleteUser(id);
-
-      return new Response("User is deleted", {
-        status: 200,
-      });
-    } catch (err) {
-      console.error("Error deleting user:", err);
-      return new Response("Error occured", {
-        status: 500,
-      });
+    catch(error)
+    {
+        console.error('Error creating or updating user:', error);
+        return new Response('Error occured in creating or updating user', {status: 500});
     }
-  }
+ }
+
+
+ if(eventType === 'user.deleted')
+ {
+    try
+    {
+        const { id } = evt?.data;
+        await deleteUser(id);
+
+        return new Response('User deleted', {status: 200});
+    }
+    catch(error)
+    {
+        console.error('Error deleting user:', error);
+        return new Response('Error occured in deleting user', {status: 500});
+    }
+ }
+
 }
