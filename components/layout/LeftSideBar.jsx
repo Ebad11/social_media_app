0acfbdc466c2827160adac9b22b231c2d3af450d
Ebad11 <ebadshk48@gmail.com>
2024-07-24 09:44:55 +0530

    updated navbar

--- components/layout/LeftSideBar.jsx
+++ components/layout/LeftSideBar.jsx
@@ -1,12 +1,40 @@
 "use client"
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Menu from './Menu'
-import { SignedIn, SignOutButton, UserButton } from '@clerk/nextjs';
+import { SignedIn, SignOutButton, UserButton, useUser } from '@clerk/nextjs';
 import { Logout } from '@mui/icons-material';
+import { dark } from '@clerk/themes';
+import Loader from '../Loader';
+
+
 const LeftSideBar = () => {
-  return (
+
+  const {user, isLoaded} = useUser();
+  const [loading, setLoading] = useState(true);
+  const [userData, setUserData] = useState({});
+
+  const getUser = async () => {
+    console.log(user);
+    const response = await fetch(`api/user/${user.id}`);
+    const data = await response.json();
+    console.log("data:",data)
+    setUserData(data);
+    setLoading(false);
+  }
+
+
+  useEffect(() => {
+    if(isLoaded) {
+
+     getUser();
+    }
+  }, [user])
+  
+  // console.log(user);
+
+  return loading || !isLoaded ? <Loader/> :  (
     <div className='h-screen left-0 top-0 sticky overflow-auto px-10 py-6 flex flex-col gap-6 max-md:hidden custom-scrollbar'>
       <Link href='/'>
         <Image src='/assets/logo.png' alt='logo' width={200} height={200} />
@@ -15,21 +43,21 @@ const LeftSideBar = () => {
       <div className="flex flex-col gap-2">
         <div className="flex flex-col gap-2 items-center text-light-1">
           <Link href='/'>
-            <Image src='/assets/phucmai.png' className='rounded-full' alt='profile photo' width={50} height={50} />
+            <Image src={userData?.profilePhoto} className='rounded-full' alt='profile photo' width={50} height={50} />
           </Link>
-          <p className='text-small-bold'>Phuc Mai</p>
+          <p className='text-small-bold'>{userData?.firstName} {userData?.lastName}</p>
         </div>
         <div className="flex justify-between text-light-1">
           <div className="flex flex-col items-center">
-            <p className='text-base-bold'>1</p>
+            <p className='text-base-bold'>{userData?.posts?.length}</p>
             <p className='text-tiny-medium' >Posts</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className='text-base-bold'>0</p>
+            <p className='text-base-bold'>{userData?.followers?.length}</p>
             <p className='text-tiny-medium' >Followers</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className='text-base-bold'>0</p>
+            <p className='text-base-bold'>{userData?.following?.length}</p>
             <p className='text-tiny-medium' >Following</p>
           </div>
         </div>
@@ -42,7 +70,7 @@ const LeftSideBar = () => {
       <hr />
 
       <div className='flex gap-4 items-center'>
-        <UserButton/>
+        <UserButton appearance={{baseTheme:dark}}/>
         <p className='text-light-1 text-body-bold'>Manage Account</p>
       </div>
 
